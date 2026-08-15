fff8dbf7661d18532b3caeb41ccb53c3b271bb31
Gabriel De Almeida Florio <gabriede@c1r3p1.42sp.org.br>
2026-08-14 21:07:43 -0300

    start default.conf

--- inc/network/ServerManager.hpp
+++ inc/network/ServerManager.hpp
@@ -2,11 +2,11 @@
 #define SERVERMANAGER_HPP
 
 #include <vector>
-#include "ServerConfig.hpp"
+#include "./ServerConfig.hpp"
 
 class ServerManager {
 private:
-    std::vector<ServerConfig> _configs;
+    std::vector<Server> _configs;
     std::vector<int> _serverSockets;
 	std::map<int, Client> _clients;
 
@@ -18,7 +18,7 @@ public:
     ServerManager& operator=(const ServerManager& other);
     ~ServerManager();
 
-    void init(const std::vector<ServerConfig>& configs); // Função responsável por inicializar o Servidor
+    void init(const std::vector<Server>& configs); // Função responsável por inicializar o Servidor
 
     void run(); // O loop infinito do poll() (ou epoll/kqueue) - Nunca deve bloquear!
 };
